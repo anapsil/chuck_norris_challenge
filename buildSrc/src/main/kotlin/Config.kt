35c20f8f7f6135c95273358da4fca4a529963e24
Ana Paula da Silva <ana.paula@bancointer.com.br>
2020-08-22 14:33:56 -0300

    Changes Config.kt variables to constants

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,26 +1,29 @@
 object Config {
-    val kotlinVersion = "1.4.0"
+    const val kotlinVersion = "1.4.0"
 
     object BuildPlugins {
-        val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}"
-        val androidGradlePlugin = "com.android.tools.build:gradle:4.0.1"
+        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        const val androidGradlePlugin = "com.android.tools.build:gradle:4.0.1"
     }
+
     object Android {
-        val minSdkVersion = 21
-        val targetSdkVersion = 29
-        val compileSdkVersion = 29
-        val applicationId = "dev.anapsil.chucknorris"
-        val versionCode = 1
-        val versionName = "1.0"
+        const val minSdkVersion = 21
+        const val targetSdkVersion = 29
+        const val compileSdkVersion = 29
+        const val applicationId = "dev.anapsil.chucknorris"
+        const val versionCode = 1
+        const val versionName = "1.0"
     }
+
     object Libs {
-        val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-        val androidCoreKtx = "androidx.core:core-ktx:1.3.1"
-        val androidAppCompat = "androidx.appcompat:appcompat:1.2.0"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+        const val androidCoreKtx = "androidx.core:core-ktx:1.3.1"
+        const val androidAppCompat = "androidx.appcompat:appcompat:1.2.0"
     }
+
     object TestLibs {
-        val junit = "junit:junit:4.12"
-        val androidJunitExtension = "androidx.test.ext:junit:1.1.1"
-        val androidEspresso = "androidx.test.espresso:espresso-core:3.2.0"
+        const val junit = "junit:junit:4.12"
+        const val androidJunitExtension = "androidx.test.ext:junit:1.1.1"
+        const val androidEspresso = "androidx.test.espresso:espresso-core:3.2.0"
     }
 }
