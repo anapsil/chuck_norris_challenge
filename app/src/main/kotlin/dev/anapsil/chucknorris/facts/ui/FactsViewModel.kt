d9f39cc5c24c1f9ce4dc41aee5311871aa936b11
Ana Silva <me@anapsil.dev>
2020-08-23 13:32:57 -0300

    Adds label uncategorized (#9)
    * Closes #4 - Adds uncategorized label for fact with no category
    
    * Closes #4 - Adds uncategorized label for fact with no category

--- app/src/main/kotlin/dev/anapsil/chucknorris/facts/ui/FactsViewModel.kt
+++ app/src/main/kotlin/dev/anapsil/chucknorris/facts/ui/FactsViewModel.kt
@@ -7,5 +7,16 @@ import dev.anapsil.chucknorris.facts.data.model.FactModel
 class FactsViewModel : ViewModel() {
     val facts = MutableLiveData<List<FactModel>>()
 
-    fun loadFacts() = Unit
+    fun loadFacts() {
+        facts.value = listOf(
+            FactModel(
+                "fnaksjdfasfas",
+                "https://www.google.com",
+                "Chuck Norris roundhouse kicked a man in a wheelchair in the ground people dug that fossil up it is now a " +
+                        "handicap parking sign but really it is a warning that Chuck is coming",
+                listOf("history")
+            ),
+            FactModel("fankljfajsdacsj", "https://www.google.com", "Religons: HINDU JEWISH CHUCK NORRIS", listOf())
+        )
+    }
 }
